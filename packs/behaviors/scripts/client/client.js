88a82ce700c5f7448bbc0be9d981027ae9c3ef9d
mrflippy <mrflippy@tresgeek.net>
2019-06-19 22:57:28 -0600

    Add multiple textures, update code and dynamic textures

--- packs/behaviors/scripts/client/client.js
+++ packs/behaviors/scripts/client/client.js
@@ -1,11 +1,51 @@
 var clientSystem = client.registerSystem(0, 0);
+var queries = {};
 
 // Setup which events to listen for
 clientSystem.initialize = function () {
 	// set up your listenToEvents and register client-side components here.
+
+    queries.inventoryQuery = clientSystem.registerQuery();
+
+    //clientSystem.listenForEvent("minecraft:client_entered_world", (eventData) => clientSystem.onClientEnter(eventData));
+    clientSystem.listenForEvent("bookshelf_storage:inventory_update", (eventData) => clientSystem.onInventoryUpdate(eventData));
 }
 
 // per-tick updates
 clientSystem.update = function () {
 	// Any logic that needs to happen every tick on the client.
+
+}
+
+clientSystem.chat = function (message) {
+    const eventData = clientSystem.createEventData("minecraft:display_chat_event");
+    eventData.data.message = message;
+    clientSystem.broadcastEvent("minecraft:display_chat_event", eventData);
+};
+
+clientSystem.onClientEnter = function (eventData) {
+    let ents = clientSystem.getEntitiesFromQuery(queries.inventoryQuery);
+
+    ents.forEach(ent => {
+        //update inventory index molang variable to 0 to start
+        if (ent.__identifier__ === "bookshelf_storage:bookshelf_storage_entity") {
+            let molang = clientSystem.getComponent(ent, "minecraft:molang");
+            molang.data["variable.bookshelfstorageinventoryindex"] = 0;
+            clientSystem.applyComponentChanges(ent, molang);
+        }
+    });
+    
+}
+
+clientSystem.onInventoryUpdate = function (eventData) {
+    let ents = clientSystem.getEntitiesFromQuery(queries.inventoryQuery);
+
+    let ent = ents.find(e => e.__unique_id__["64bit_low"] === eventData.data.entity_uid["64bit_low"] && e.__unique_id__["64bit_high"] === eventData.data.entity_uid["64bit_high"]);
+
+    //update the molang variable
+    if (ent) {
+        let molang = clientSystem.getComponent(ent, "minecraft:molang");
+        molang.data["variable.bookshelfstorageinventoryindex"] = eventData.data.inventory_fullness;
+        clientSystem.applyComponentChanges(ent, molang);
+    }
 }
--- packs/behaviors/scripts/server/server.js
+++ packs/behaviors/scripts/server/server.js
@@ -1,8 +1,14 @@
 var serverSystem = server.registerSystem(0, 0);
+var queries = {};
+var inventoryCache = {};
 
 // Setup which events to listen for
 serverSystem.initialize = function () {
 	// set up your listenToEvents and register server-side components here.
+
+    queries.inventoryQuery = serverSystem.registerQuery();
+
+    serverSystem.registerEventData("bookshelf_storage:inventory_update", { entity_uid: {}, inventory_fullness: 0 });
 
     serverSystem.listenForEvent("minecraft:player_placed_block", (eventData) => serverSystem.onPlacedBlock(eventData));
 }
@@ -10,6 +16,15 @@ serverSystem.initialize = function () {
 // per-tick updates
 serverSystem.update = function () {
 	// Any logic that needs to happen every tick on the server.
+    //update bookshelf inventory levels
+    let ents = serverSystem.getEntitiesFromQuery(queries.inventoryQuery);
+
+    ents.forEach(ent => {
+        //check bookshelf inventory level
+        if (ent.__identifier__ === "bookshelf_storage:bookshelf_storage_entity") {
+            checkInventoryLevel(ent);
+        }
+    });
 }
 
 serverSystem.chat = function (message) {
@@ -45,13 +60,47 @@ function spawnBookshelf(position) {
     let bookshelf = serverSystem.createEntity("entity", "bookshelf_storage:bookshelf_storage_entity");
     if (bookshelf !== null) {
         //set the position
+        //adjust entity position to be in the middle of the block square
         let bookshelf_position = serverSystem.getComponent(bookshelf, "minecraft:position");
-        bookshelf_position.data.x = position.x;
+        bookshelf_position.data.x = position.x + 0.5;
         bookshelf_position.data.y = position.y;
-        bookshelf_position.data.z = position.z;
+        bookshelf_position.data.z = position.z + 0.5;
 
         serverSystem.applyComponentChanges(bookshelf, bookshelf_position);
 
         //serverSystem.chat("Spawned bookshelf")
+    }
+}
+
+function checkInventoryLevel(bookshelf_entity) {
+    let inventory = serverSystem.getComponent(bookshelf_entity, "minecraft:inventory").data;
+    let inventoryContainer = serverSystem.getComponent(bookshelf_entity, "minecraft:inventory_container").data;
+
+    //currently only have 0, 1, 2
+    let fullness = 0
+    let inventorySize = inventoryContainer.filter(e => e.item !== "minecraft:undefined").length;
+
+    //count the number of slots that are not empty
+    if (inventorySize >= inventory.inventory_size) {
+        fullness = 2;
+    } else if (inventorySize > 0) {
+        fullness = 1
+    }
+
+    //
+    let firstTime = false;
+    if (!inventoryCache.hasOwnProperty(bookshelf_entity.id)) {
+        inventoryCache[bookshelf_entity.id] = fullness;
+        firstTime = true;
+    }
+
+    //only update if changed
+    if (inventoryCache[bookshelf_entity.id] !== fullness || firstTime) {
+        inventoryCache[bookshelf_entity.id] = fullness;
+        let inventoryUpdateData = serverSystem.createEventData("bookshelf_storage:inventory_update")
+        inventoryUpdateData.data.entity_uid = bookshelf_entity.__unique_id__;
+        inventoryUpdateData.data.inventory_fullness = fullness;
+
+        serverSystem.broadcastEvent("bookshelf_storage:inventory_update", inventoryUpdateData);
     }
 }
